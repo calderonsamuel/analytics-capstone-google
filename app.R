cbc5e800eb30322d18333b11fae51574f9d5f67d
calderonsamuel <samuel.calderon@uarm.pe>
2023-01-11 13:11:20 -0500

    separate plotlyfy

--- app.R
+++ app.R
@@ -16,37 +16,6 @@ choices_condicion <- levels(investigadores$condicion)
 choices_nivel <- levels(investigadores$nivel)
 choices_genero <- levels(investigadores$genero)
 
-plotlyfy <- function(data, .var, title = "") {
-    tbl <- data |> 
-        count({{ .var }}) |> 
-        rename(column = {{ .var }}) |> 
-        mutate(
-            percent = (n/sum(n)*100) |> round(1),
-            text = paste0(n, " (", percent, "%)")
-        )
-    
-    # fake environment. otherwise plot_ly errors (?)
-    (\() {
-        tbl |>
-            plot_ly(
-                x = ~n,
-                y = ~column,
-                type = "bar",
-                text = ~text,
-                textposition = 'auto',
-                marker = list(color = 'rgb(158,202,225)',
-                              line = list(color = 'rgb(8,48,107)', width = 1.5))
-            ) |>
-            config(displayModeBar = F) |> 
-            layout(
-                title = title,
-                yaxis = list(title = ""),
-                xaxis = list(title = "")
-            )
-    })()
-    
-}
-
 ui <- page_fluid(
     title = "RENACYT",
     class = "p-3",

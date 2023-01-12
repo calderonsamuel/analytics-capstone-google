ad9a783d4c51fd69ef067676019079720ad7d5c7
calderonsamuel <samuel.calderon@uarm.pe>
2023-01-11 19:30:57 -0500

    bye

--- inst/analysis.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-investigadores <- read_rds("data/investigadores_clean.rds")
-
-investigadores |> 
-    filter(str_detect(institucion_principal, "UNIVERSIDAD")) |> 
-    count(institucion_principal, sort = T)
-
-
